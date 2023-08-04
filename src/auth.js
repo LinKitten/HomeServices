b11ee4bbecbcf928f3f76dae42199aa372a1bf1f
LinKitten <2634024527@qq.com>
2023-08-04 09:32:52 +0800

    追加实现用户信息是否正确

--- src/auth.js
+++ src/auth.js
@@ -10,28 +10,61 @@ class AuthRouter extends React.Component {
 
 
     render() {
-
+        // 获取cookie上的用户信息
+        var cookie = React.$Cookies.load("business") ? React.$Cookies.load("business") : {}
         const AuthLogin = () => {
-            // 初始化navigate
-            let navigate = React.$Router.useNavigate();
+            // 实例化 
+            var navigate = React.$Router.useNavigate();
+
+
+            if (Object.getOwnPropertyNames(cookie).length <= 0) {
+                React.$Vant.Toast.fail({
+                    message: "请先登录",
+                    onClose: () => {
+                        navigate("/login")
+                    }
+                })
+            }
 
             React.useEffect(() => {
-                // 获取cookie上的用户信息
-                var cookie = React.$Cookies.load("business") ? React.$Cookies.load("business") : {}
+                check();
 
-                if (Object.getOwnPropertyNames(cookie).length <= 0) {
+            })
+            const check = async () => {
+                var data = {
+                    id: cookie.id,
+                    mobile: cookie.mobile
+                }
+    
+    
+                // 发起请求，确认cookie信息是否正确
+                var result = await React.$HTTP.POST({
+                    url: "/business/check",
+                    params: data
+                });
+    
+                if (result.code == 0) {
+                    // 非法登录，需要删除cooike
+    
+                    // 删除cookie
+                    React.$Cookies.remove('business', { path: '/' });
+    
                     React.$Vant.Toast.fail({
-                        message: "请先登录",
+                        message: result.msg,
                         onClose: () => {
-                            navigate("/login")
+                            navigate(result.url)
                         }
                     })
                 }
-
-            })
+    
+                //保存用户信息到cookie
+                React.$Cookies.save("business", result.data, { path: '/' })
+            }
+    
 
         }
 
+  
 
 
         return (
